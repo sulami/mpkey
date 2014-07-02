2996d3c200458f2bb50bbb749471632261b85ed7
Robin Schroer <sulamiification@gmail.com>
2014-07-02 13:02:55 +0200

    added xcb connection

--- mpkey.c
+++ mpkey.c
@@ -7,10 +7,16 @@
 int main()
 {
     struct mpd_connection *mpd;
+    struct xcb_connection_t *xcb;
+    int screen;
 
     mpd = mpd_connection_new(HOST, PORT, TIMEOUT);
     if (!mpd)
-        exit(1);
+        return -1;
+
+    if (xcb_connection_has_error((xcb = xcb_connect(NULL, &screen))))
+        return -2;
+
     return 0;
 }
 
