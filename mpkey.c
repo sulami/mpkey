94e1ac1423cfbe059922d6f9a8401c712ca7eca3
Robin Schroer <sulamiification@gmail.com>
2014-07-02 13:36:43 +0200

    added xcb_screen_of_display helper (from frankenwm)

--- mpkey.c
+++ mpkey.c
@@ -4,6 +4,19 @@
 
 #include "config.h"
 
+/* get screen of display */
+static xcb_screen_t *xcb_screen_of_display(xcb_connection_t *con, int screen)
+{
+    xcb_screen_iterator_t iter;
+
+    iter = xcb_setup_roots_iterator(xcb_get_setup(con));
+    for (; iter.rem; --screen, xcb_screen_next(&iter))
+        if (screen == 0)
+            return iter.data;
+
+    return NULL;
+}
+
 int main()
 {
     struct mpd_connection *mpd;
